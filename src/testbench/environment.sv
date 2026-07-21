55e235b0c8ab3dd1969e6ab4038216ed1ad89097
Reddygari Shanmukha reddy <shanmukhareddyshan@gmail.com>
2026-07-21 19:05:44 +0530

    Update environment.sv

--- src/testbench/environment.sv
+++ src/testbench/environment.sv
@@ -9,19 +9,23 @@ class environment;
     mailbox #(transaction) mbx_dr;
     mailbox #(transaction) mbx_rs;
     mailbox #(transaction) mbx_ms;
-    virtual inf vif;
-    function new(virtual inf vif);
-        this.vif = vif;
+    virtual inf.DRV drv_vif;
+    virtual inf.MON mon_vif;
+    function new(virtual inf.MON mon_vif,virtual inf.DRV drv_vif);
+        this.drv_vif = drv_vif;
+        this.mon_vif = mon_vif;
+    endfunction
+   task build();
         mbx_gd = new();
         mbx_dr = new();
         mbx_rs = new();
         mbx_ms = new();
         gen  = new(mbx_gd);
-        drv  = new(mbx_gd, mbx_dr, vif);
+        drv  = new(mbx_gd, mbx_dr, drv_vif);
         refm = new(mbx_dr, mbx_rs);
-        mon  = new(mbx_ms, vif);
+        mon  = new(mbx_ms, mon_vif);
         sb   = new(mbx_rs, mbx_ms);
-    endfunction
+   endtask 
     task start();
         fork
             gen.start();
@@ -29,9 +33,6 @@ class environment;
             refm.start();
             mon.start();
             sb.start();
-        join_any
-        @(posedge vif.clk);
-        $display("\n=============================");
-        $display("       SIMULATION COMPLETED");
+        join
     endtask
 endclass
