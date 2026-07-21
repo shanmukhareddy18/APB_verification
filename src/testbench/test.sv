f822fa382dafdd35a84bf9f33b4f724d6e9828af
Reddygari Shanmukha reddy <shanmukhareddyshan@gmail.com>
2026-07-21 19:02:51 +0530

    Update test.sv

--- src/testbench/test.sv
+++ src/testbench/test.sv
@@ -1,17 +1,89 @@
 `include "defines.svh"
-
 class test;
     environment env;
-    virtual inf vif;
-    function new(virtual inf vif);
-        this.vif = vif;
-        env = new(vif);
+    virtual inf.DRV drv_vif;
+    virtual inf.MON mon_vif;    
+function new(virtual inf.DRV drv_vif,virtual inf.MON mon_vif);
+        this.drv_vif = drv_vif;
+	this.mon_vif=mon_vif;
     endfunction
-    task run();
-        $display("        APB MASTER TEST STARTED");
-        $display("====================================");
+/*    task run();
+       env=new(mon_vif,drv_vif);
+       env.build();
+        $display("\n========================================");
+        $display(" SIMULATION STARTED");
+        $display("========================================");
         env.start();
-        $display("\======================================");
-        $display("        APB MASTER TEST PASSED");
-    endtask
+    endtask */
+endclass
+
+/* class test1 extends test;
+	transaction1 trans1;
+  	function new(virtual inf.DRV drv_vif,virtual inf.MON mon_vif);
+    		super.new(drv_vif,mon_vif);
+  	endfunction
+  	task run();
+    		env=new(mon_vif,drv_vif);
+    		env.build();
+    		begin
+    			trans1 = new();
+    			env.gen.blueprint= trans1;
+    		end
+                $display("\n========================================");
+              $display(" TEST=1 SIMULATION STARTED");
+             $display("========================================");
+
+    		env.start();
+  	endtask
+endclass
+
+class test2 extends test;
+        transaction2 trans2;
+        function new(virtual inf.DRV drv_vif,virtual inf.MON mon_vif);
+                super.new(drv_vif,mon_vif);
+        endfunction
+        task run();
+                env=new(mon_vif,drv_vif);
+                env.build();
+                begin
+                        trans2 = new();
+                        env.gen.blueprint= trans2;
+                end
+                $display("\n========================================");
+              $display(" TEST=2 SIMULATION STARTED");
+             $display("========================================");
+
+                env.start();
+        endtask
+endclass */
+
+class test_regression extends test;
+ transaction1 t1;
+ transaction2 t2;
+ transaction3 t3;
+ function new(virtual inf.DRV drv_vif,virtual inf.MON mon_vif);
+  super.new(drv_vif,mon_vif);
+endfunction
+task run();
+env=new(mon_vif,drv_vif);
+env.build;
+begin
+t1 =new();
+env.gen.blueprint=t1;
+end
+env.start;
+
+begin
+t2 =new();
+env.gen.blueprint=t2;
+end
+env.start;
+
+begin
+t3 =new();
+env.gen.blueprint=t2;
+end
+env.start;
+
+endtask
 endclass
